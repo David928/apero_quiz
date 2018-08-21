8680673eeef633c0d1a6b0c7bd386e124d000824
David928 <david.gonzales928@gmail.com>
2018-08-21 23:09:13 +0200

    bord arrondi

--- Apero Quiz/ViewController.swift
+++ Apero Quiz/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var Container: UIView!
+    
+    var carte: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        carte = UIView(frame: CGRect(
+            x: 50,
+            y: Container.frame.height - 200,
+            width: 200,
+            height: 200))
+        carte?.backgroundColor = UIColor.white
+        carte?.layer.cornerRadius = 50
+        Container.addSubview(carte ?? UIView())
     }
 
 
